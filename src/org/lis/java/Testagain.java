fdfd2d23beb4307619490864315688be80046afb
mieky <359502980@qq.com>
2016-04-07 00:05:46 +0800

    ceshi

--- src/org/lis/java/Testagain.java
+++ src/org/lis/java/Testagain.java
@@ -3,6 +3,9 @@ package org.lis.java;
 public class Testagain {
 
 	public static void main(String[] args) {
+		int a=0;
+		int b = 2;
+		System.out.println(a+b);
 		System.out.println("Test Again!");
 	}
 
